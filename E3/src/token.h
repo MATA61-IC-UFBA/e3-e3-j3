3aad50a5772e1ec0bf601ebc55a4ec59f9430347
JoaoRCSJ <154098974+JoaoRCSJ@users.noreply.github.com>
2024-08-14 13:44:41 -0300

    Update token.h

--- E3/src/token.h
+++ E3/src/token.h
@@ -2,10 +2,11 @@
 
 typedef enum {
         ID = 256, 
+	IF, ELSE, FOR, WHILE, RETURN, FUNCTION, BOOLEAN_TYPE, INTEGER_TYPE, CHAR_TYPE, STRING_TYPE, TRUE, FALSE, VOID, ARRAY, // Keywords
+    IDENTIFIER, 
+ LE, GE, EQ, NEQ, AND, OR, // Operadores compostos
+    PLUS, MINUS, TIMES, DIV, MOD, NOT, LT, GT, ASSIGN, COLON, SEMICOLON, COMMA, LPAREN, RPAREN, LBRACKET, RBRACKET, LBRACE, RBRACE, BACKSLASH, // Operadores e símbolos simples
 	NUM, 
-        KEY,
-        SYM,  
-        SYMC, 
         CHAR,
 	STR,
 	ERROR
